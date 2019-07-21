557327b30e3e15d2738309e0bd229dd2f9e9c133
devPinheiro <pinheirolaoluwa@gmail.com>
2019-07-21 23:54:51 +0100

    feature(landing-page): setup landing page [Finishess #167072629]

--- client/pages/Main.js
+++ client/pages/Main.js
@@ -1,21 +1,24 @@
-import React, { Component } from "react";
-import { BrowserRouter, Route } from "react-router-dom";
-import Header from "../layout/Header";
-import Footer from "../layout/Footer";
-import Landing from "../pages/Home";
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Header from '../layout/Header';
+import Footer from '../layout/Footer';
+import Landing from '../pages/Home';
 
 class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <div>
-          <Header />
-          <Route component={Landing} exact path="/" />
-          <Footer />
-        </div>
-      </BrowserRouter>
-    );
-  }
+    render() {
+        return (
+           <Router>
+               <Header />
+                    <Switch>
+                        <Route component={Landing} exact path="/" />
+                    </Switch>
+               <Footer />
+           </Router>
+                
+           
+        )
+    }
 }
+
 
 export default App;
