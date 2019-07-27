c7574f827b0731506e77278b9bd6fc8d63f94858
devPinheiro <pinheirolaoluwa@gmail.com>
2019-07-27 04:21:25 +0100

    chore(deploy): deploy app to Heroku setup express server and add heroku postbuild script [Finishes #167061555]

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "./dist"),
     filename: "app.js",
-    publicPath: "/"
+    publicPath: "/dist/"
   },
   module: {
     rules: [
